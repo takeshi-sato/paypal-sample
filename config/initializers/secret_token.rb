3b998247b63c9c58ba30108dee08f4df2e52ed93
Takeshi Sato <takeshi-sato@netyear.net>
2011-07-25 16:52:06 +0900

    first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaypalSample::Application.config.secret_token = 'b07b9f18845dae375d053236b5643fb46da04608349e5576e082e4356ff781f52f51f1ad372f2fe8ae1dd755329c87d8328805a9fa55e84541c6ac4431c2c275'
