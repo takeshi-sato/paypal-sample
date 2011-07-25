379c3396988b34d8f889e2f30a140ec70245b169
Takeshi Sato <takeshi-sato@netyear.net>
2011-07-25 17:57:54 +0900

    orderをscaffoldで生成。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  filter_parameter_logging :card_number, :card_verification
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 PaypalSample::Application.routes.draw do
+  resources :orders
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
